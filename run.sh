9d8494f3cc61e08e9838440f2c66f8da94121a8b
Paulo Cesar Cuneo <cuneo.paulo.cesar@gmail.com>
2021-05-03 23:32:47 -0300

    create work folder if not exists

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
-
+mkdir -p work
 docker build . -t pycaret-notebook
 docker run --rm -p 8888:8888 -e JUPYTER_ENABLE_LAB=yes -v "$PWD"/work:/home/jovyan/work pycaret-notebook
 
